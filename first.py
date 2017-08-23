dc3dfdc96e7cce6a5c4e63b02261cfd9379ee3c5
khoser <ktuuuu@gmail.com>
2017-08-23 09:59:16 +0400

    Comments

--- first.py
+++ first.py
@@ -12,32 +12,39 @@ except ImportError:
 	import xml.etree.ElementTree as etree
 
 
-rand = RR().random
+rand = RR().random #для короткого вызова случайного числа
 
-csv1=[]
+csv1=[] #данные для сохранения csv файлов
 csv2=[]
 	
 
+"""
+Функция формирует уникальное "случайное" строковое значение
+"""
 def randunic():
 	m = md5()
 	m.update(str(datetime.now().isoformat()).encode('utf-8'))
 	return m.hexdigest()
 
 
+"""
+Случайное строковое значение
+"""
 def rands():
 	m = md5()
 	m.update(str(rand()).encode('utf-8'))
 	return m.hexdigest()
 
 
+"""
+xml-данные для сохранения в файл
+"""
 def newXML():
-	global max10
-	global max100
 	newroot = etree.Element("root") # рутовый элемент 
 	newvar = etree.SubElement(newroot, "var") # добавляем дочерний элемент к root 
 	newvar.set('name', u"id") # добавляем значение элемента
 	newvar.set('value', randunic()) # добавляем значение элемента
-	newvar = etree.SubElement(newroot, "var") # добавляем дочерний элемент к root 
+	newvar = etree.SubElement(newroot, "var") # добавляем дочерний элемент к root
 	newvar.set('name', u"level") # добавляем значение элемента
 	tmp=1+int(rand()*100)
 	newvar.set('value', str(tmp)) # добавляем значение элемента
@@ -49,43 +56,48 @@ def newXML():
 		newobject.set('name', rands()) # добавляем значение
 		
 	message = etree.tostring(newroot, "utf-8") # формируем XML документ в строку message
-	#doc = '' + message # Добавляем строчку кодировки для xml файла
-	###################### выводим результат формирования xml на экран ############
-	#print message
-	#print doc#.decode('utf-8') #меняем кодировку чтобы под виндовс было понятно что написано русскими буквами 
 	return message
 
 
+"""
+Сохранение одного файла xml
+"""
 def fsave(name, data):
 	outputfile = open(r''+name+'.xml', "wb")
 	outputfile.write(data)
 	outputfile.close()
 
 
+"""
+Основной алгоритм первой части задания
+"""
 def createData():
 	try:
-		os.makedirs('data')
-		os.chdir('data')
+		os.makedirs('data') #Создаем и переходим в новую папку. Папка на момент запуска программы должна отсутствовать.
 	except OSError:
 		pass
+	os.chdir('data')
 	for i in range(50):
 		numer = str(i)
-		os.makedirs(numer)
-		z = zipfile.ZipFile(numer + '.zip', 'w')        # Создание нового архива
+		os.makedirs(numer) #Создаем временную папку
+		z = zipfile.ZipFile(numer + '.zip', 'w')        # Создание очередного архива из 50
 		os.chdir(numer)
 		for f in range(100):
 			fsave(str(f), newXML())
-			z.write(os.path.join('',str(f)+'.xml'))         # Создание относительных путей и запись файлов в архив
+			z.write(os.path.join('',str(f)+'.xml'))         # Создание очередного файла xml и запись его в архив
 		z.close()
 		for root, dirs, files in os.walk('.'): # Список всех файлов и папок в директории
 			for file in files:
-				os.remove(file)
+				os.remove(file) #Удаляем временные файлы
 		os.chdir('..')
-		os.removedirs(numer)
+		os.removedirs(numer) #Удаляем временную папки
 	os.chdir('..')
 	print('First step done')
 
 
+"""
+Сохранение файлов csv на диск
+"""
 def scsv():
 	with open('csv1.csv', 'w') as csvfile:
 		writer = csv.writer(csvfile)
@@ -95,37 +107,42 @@ def scsv():
 		writer.writerows(csv2)
 		
 
+"""
+Анализ xml файла
+"""
 def parsedata(data):
 	global csv1
 	global csv2
-	oldroot = etree.fromstring(data)
+	oldroot = etree.fromstring(data) #Парсим переданную строку
 	for vari in oldroot.findall('var'):
 		for vv in oldroot.findall('var'):
 			if vv.get('name')=='id':
 				id = vv.get('value')
 			if vv.get('name')=='level':
 				level=vv.get('value')
-	csv1.append([id,level])
+	csv1.append([id,level]) #Записываем в первый параметр для последующего сохранения
 	for obj in oldroot.iter('object'):
-		csv2.append([id, obj.get('name')])
+		csv2.append([id, obj.get('name')]) #записываем во второй параметр
 	
 
+"""
+Основной алгоритм второй части задачи
+"""
 def analyseData():
 	#os.chdir('data')
 	for root, dirs, files in os.walk('data'): # Список всех файлов и папок в директории data
 		for file in files:
-			z = zipfile.ZipFile(os.path.join(root,file), 'r')
+			z = zipfile.ZipFile(os.path.join(root,file), 'r') #Открываем архив на чтение
 			for xml in z.namelist():
-				data = z.read(xml)
-				parsedata(data)
-	scsv()
+				data = z.read(xml) # чтение данных конкретного файла архива
+				parsedata(data) # парсинг
+	scsv() # сохранение
 
 
 def main():
-	createData()
+	createData() # первая часть
 	input('Press any key for second step..')
-	analyseData()
-	
+	analyseData() # вторая часть
 	return 0
 
 
